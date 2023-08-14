bf24aee38cfa96a716c2de7b2fe0d4bad2834bf6
FranciscoSaldivar21 <francisco.saldivar4081@alumnos.udg.mx>
2023-08-14 09:05:59 -0600

    PORT updated 2

--- src/database/config.js
+++ src/database/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 
 config();
 
-export const SERVER_PORT = process.env.SERVER_PORT;
+export const SERVER_PORT = process.env.PORT;
 export const HOST = process.env.HOST;
 export const USER = process.env.USER;
 export const PASSWORD = process.env.PASSWORD;
